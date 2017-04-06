0626efe5c210fe519dcfbcc4de3043852b71f9d5
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-04-06 11:33:10 -0500

    add boost::mpi point-to-point tutorial

--- boostMpiApp.cpp
+++ boostMpiApp.cpp
@@ -17,12 +17,42 @@
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
 
-int main(int argc, char **argv)
+#include <iostream>
+#include <string>
+
+void tutorial1()
 {
-  //boost::mpi::environment env(argc, argv);
   boost::mpi::environment env;
   boost::mpi::communicator world;
   std::cout << "I am process " << world.rank() << " of " << world.size()
             << "." << std::endl;
+}
+
+int tutorial2()
+{
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+
+  auto rank = world.rank();
+
+  if (rank == 0) {
+    world.send(1, 0, std::string("Hello"));
+    std::string msg;
+    world.recv(1, 1, msg);
+    std::cout << msg << "!" << std::endl;
+  } else if (rank == 1) {
+    std::string msg;
+    world.recv(0, 0, msg);
+    std::cout << msg << ", ";
+    std::cout.flush();
+    world.send(0, 1, std::string("world"));
+  }
+}
+
+int main(int argc, char **argv)
+{
+  //tutorial1();
+  tutorial2();
+
   return 0;
 }
